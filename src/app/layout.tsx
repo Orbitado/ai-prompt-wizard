886241267f1040f96e3436131b027fe013e5897b
Orbitadoo <juanpaavaldezz@gmail.com>
2024-09-30 11:48:33 -0300

    [ADDED] Hotfix NavBar, Button && Prompts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import NavBar from "@/components/specific/NavBar/NavBar";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 400 500 900",
 });
 
 export const metadata: Metadata = {
