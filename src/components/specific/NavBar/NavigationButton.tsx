b27f88a534a9606436c84f95104d088b06af0ac9
Leonardo Juan Pablo Valdez <105684685+Orbitado@users.noreply.github.com>
2024-09-30 12:48:40 -0300

    [ADDED] handleOutsideClick function to close the NavBar when Users click outside. (#6)

--- src/components/specific/NavBar/NavigationButton.tsx
+++ src/components/specific/NavBar/NavigationButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 import MobileMenu from "@/components/specific/NavBar/MobileMenu";
 import Link from "next/link";
@@ -9,6 +9,16 @@ export default function NavigationButton() {
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    const handleClickOutside = () => {
+      if (isOpen) {
+        setIsOpen(true);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [isOpen]);
+
   return (
     <>
       <div className="flex justify-between items-center">
