8dd8664c6653386320077e64048d4d4f507f9fc6
Orbitado <juanpaavaldezz@gmail.com>
2024-10-04 16:22:33 -0300

    [EDITED] NavBar won't open alone when the user is on mobile.

--- src/components/specific/NavBar/MobileMenu.tsx
+++ src/components/specific/NavBar/MobileMenu.tsx
@@ -1,10 +1,10 @@
 import { renderLinks } from "@/services/renderNavigationLinks";
 import React from "react";
 
-export default function MobileMenu({ isOpen }: { isOpen: boolean }) {
+export default function MobileMenu({ isOpen = false }: { isOpen: boolean }) {
   return (
     <>
-      {isOpen || (
+      {isOpen && (
         <ul className="flex flex-col items-center gap-4 md:hidden my-4">
           {renderLinks()}
         </ul>
